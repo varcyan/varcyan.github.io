b451fa4d0463d02c8494ef44cd613cd158ed99f3
varcyan <varcyan@qq.com>
2017-06-20 20:22:01 +0800

    blog

--- static/js/viewport.js
+++
@@ -1,9 +0,0 @@
-// 获取像素比
-var num = 1 / window.devicePixelRatio;
-// 添加视口设置
-document.write('<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=' + num +
-  ',minimum-scale=' + num + ' ,maximum-scale=' + num + '" />');
-// 设置html基础字号
-var fontNum = document.documentElement.clientWidth / 18.75;
-var html = document.getElementsByTagName('html')[0];
-html.style.fontSize = fontNum + 'px'; //设置HTML字号
